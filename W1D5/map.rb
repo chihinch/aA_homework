cef43175a48f5a51b7c81e9cb25021000c731bf9
Danny Chan <dchan6513@gmail.com>
2019-08-16 01:55:53 -0400

    Fill in methods for Map class and define method to return  pair containing key

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,18 +1,32 @@
 class Map
 
     def initialize
+        @current_map = Array.new
     end
 
     def set(key, value)
+        if @current_map.none? {|pair| pair.first == key}
+            @current_map << [key, value]
+        else
+            puts "Key already exists!"
+        end
+        self
     end
 
     def get(key)
+        find_pair_w_key(key).first
     end
 
     def delete(key)
+        @current_map.delete(find_pair_w_key(key))
+    end
+
+    def find_pair_w_key(key)
+        @current_map.bsearch { |pair| pair.first == key }
     end
 
     def show
+        @current_map
     end
 
 end
