c68c6a27b785932a757f28d88007ba5bcb1f4a6d
Danny Chan <dchan6513@gmail.com>
2019-08-16 01:35:31 -0400

    Fill methods for Queue class

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -1,15 +1,23 @@
 class Queue
 
     def initialize
+        @current_queue = Array.new
     end
 
     def enqueue(el)
+        # Front of queue is last element
+        @current_queue.unshift(el)
+        self
     end
 
     def dequeue
+        # Remove from front of queue (i.e. last element of array)
+        @current_queue.pop
     end
 
     def peek
+        # Want to see the first person in the queue
+        @current_queue.last
     end
 
 end
