aed12662f0cdd20e0bef9eace60bc6c1d8d83176
Danny Chan <dchan6513@gmail.com>
2019-08-18 01:33:17 -0400

    Add comments to understand Mancala class

--- W2D1/skeleton/lib/mancala.rb
+++ W2D1/skeleton/lib/mancala.rb
@@ -3,9 +3,9 @@ require_relative 'player'
 
 class Mancala
   def initialize(name1, name2)
-    @player1 = Player.new(name1, 1)
-    @player2 = Player.new(name2, 2)
-    @board = Board.new(name1, name2)
+    @player1 = Player.new(name1, 1) # Create a player instance who takes side 1
+    @player2 = Player.new(name2, 2) # Create a player instance who takes side 2
+    @board = Board.new(name1, name2) # Create a new board with both players
   end
 
   def play
@@ -13,11 +13,11 @@ class Mancala
     @board.render
     current_player = @player1
     until won?
-      take_turn(current_player)
+      take_turn(current_player) # Player takes their turn (and continues until move_result is :switch)
       current_player = current_player == @player1 ? @player2 : @player1
     end
 
-    puts "The game is finished! #{winner_message}"
+    puts "The game is finished! #{winner_message}" # Prints out winner message once until loop ends
   end
 
   def take_turn(current_player)
@@ -25,19 +25,19 @@ class Mancala
 
     until move_result == :switch
       if move_result == :prompt
-        print_indices
+        print_indices # Prints the board indices
 
         begin
-          start_pos = current_player.prompt
-          start_pos -= 1 if start_pos <= 6
+          start_pos = current_player.prompt # Prompts player for move
+          start_pos -= 1 if start_pos <= 6 # Account for zero indexing?
           @board.valid_move?(start_pos)
         rescue Exception => e
           puts e.message
         retry
         end
-        move_result = @board.make_move(start_pos, current_player.name)
+        move_result = @board.make_move(start_pos, current_player.name) # Make current move from start_pos
       else
-        move_result = @board.make_move(move_result, current_player.name)
+        move_result = @board.make_move(move_result, current_player.name) # Make move from the previous result
       end
       break if won?
     end
