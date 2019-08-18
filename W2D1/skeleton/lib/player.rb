8ea522b52f7f30c433a45a15cbf966e00192dce1
Danny Chan <dchan6513@gmail.com>
2019-08-18 01:36:05 -0400

    Add comments to understand Player class

--- W2D1/skeleton/lib/player.rb
+++ W2D1/skeleton/lib/player.rb
@@ -9,9 +9,9 @@ class Player
   def prompt
     puts "#{@name}, please enter which cup you would like to start from: "
     start_pos = gets.chomp.to_i
-    if (start_pos.between?(1,6) && @side != 1 ||
-        start_pos.between?(7,12) && @side != 2)
-      raise "Not your side!"
+    if (start_pos.between?(1,6) && @side != 1 || # If player is side 1 and doesn't enter 1 to 6
+        start_pos.between?(7,12) && @side != 2) # Or if player is side 2 and doesn't enter 7 to 12
+      raise "Not your side!" # Let player know they entered an invalid start_pos
     else
       start_pos
     end
