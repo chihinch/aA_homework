500a0753d7b40bb404d9351a993f086d0b5a8cc9
Danny Chan <dchan6513@gmail.com>
2019-08-18 20:05:06 -0400

    Fix bugs in #make_move and refactor #winner in Board class

--- W2D1/skeleton/lib/board.rb
+++ W2D1/skeleton/lib/board.rb
@@ -2,49 +2,55 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @player1 = name1
-    @player2 = name2
+    @name1 = name1
+    @name2 = name2
     @cups = Array.new(14) {Array.new}
     place_stones
-    end
   end
 
   def place_stones
     cups.each_with_index do |cup, cup_index|
-      4.times {cup << :stone} unless cup_index == 6 || cup_index == 13
+      next if cup_index == 6 || cup_index == 13
+      4.times {cup << :stone}
+    end
   end
 
   def valid_move?(start_pos)
-    raise "Invalid starting cup" if !(0..13).include?(start_pos)
+    raise "Invalid starting cup" if !(0..12).include?(start_pos)
     raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
-    num_stones_in_start_cup = @cups[start_pos].length # Count how many stones there were in the selected cup
-    @cups[start_pos].clear # Empty the selected cup
+    stones = @cups[start_pos] # Gather up the stones in the starting cup
+    @cups[start_pos] = [] # Empty the starting cup
 
-    cup_index = start_pos
+    cup_index = start_pos # Start counting indices at the starting cup
 
-    until num_stones_in_start_cup == 0
+    until stones.empty? # Until the stones gathered are all distributed...
       cup_index += 1 # Advance to the next cup
-      if cup_index == 6 && current_player_name == @player1
-        cup_index = 7 # Skip cup 6 if side 1 is playing
-      elsif (cup_index == 12 && current_player_name == @player2) || cup_index > 13
-        cup_index = 0 # Skip cup 13 if side 2 is playing, or reset to cup 0 if the cup_index goes out of bounds
+      cup_index = 0 if cup_index > 13 # Reset index counter to 0 if we go past 13 
+      if cup_index == 6
+        @cups[6] << stones.pop if current_player_name == @name1 # Add a stone to cup 6 if we're at that cup and the current player is player 1
+      elsif cup_index == 13
+        @cups[13] << stones.pop if current_player_name == @name2 # Similar if we're on player 2
+      else
+        @cups[cup_index] << stones.pop
       end
-      @cups[cup_index] << :stone # Add a stone to the current cup
-      num_stones_in_start_cup -= 1
     end
 
     render
     next_turn(cup_index)
-
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
-    return :switch if @cups[ending_cup_idx].length - 1 == 0
-    return :prompt if ending_cup_idx = nil
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt 
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -60,7 +66,12 @@ class Board
   end
 
   def winner
-    return :draw if @cups[6].length == @cups[13].length
-    @cups[6].length > @cups[13].length ? @player1 : @player2
+    player1_stones = @cups[6].count
+    player2_stones = @cups[13].count
+    if player1_stones == player2_stones
+      :draw
+    else
+      player1_stones > player2_stones ? @name1 : @name2
+    end
   end
 end
