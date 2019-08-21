e4461d281be9171aab4f9487501a2882ca761505
Danny Chan <dchan6513@gmail.com>
2019-08-20 23:35:30 -0400

    Implement #add_ingredient spec

--- W2D3/dessert/spec/dessert_spec.rb
+++ W2D3/dessert/spec/dessert_spec.rb
@@ -28,7 +28,10 @@ describe Dessert do
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      cannoli.add_ingredient("flour")
+      expect(cannoli.ingredients).to eq(["flour"])
+    end
   end
 
   describe "#mix!" do
