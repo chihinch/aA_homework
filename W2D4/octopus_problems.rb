d41c1c13c6f2029b037100b2b0a92bb33f7ccd6c
Danny Chan <dchan6513@gmail.com>
2019-08-21 23:37:56 -0400

    Create skeleton for octopus_problems

---
+++ W2D4/octopus_problems.rb
@@ -0,0 +1,27 @@
+# Hungry Octopus
+
+fish = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+# O(n^2) 
+def sluggish_octopus
+end
+
+# O(n log n)
+def dominant_octopus
+end
+
+# O(n)
+def clever_octopus
+end
+
+# Dancing Octopus
+
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+# O(n)
+def slow_dance(tile, tiles_array)
+end
+
+# O(1)
+def constant_dance(tile, tiles_array)
+end
