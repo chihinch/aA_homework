8e48a54e59c2686eb4a020918daa57dda841b2d1
Danny Chan <dchan6513@gmail.com>
2019-08-23 00:48:13 -0400

    Refactor LRU cache

--- W2D5/lru_cache.rb
+++ W2D5/lru_cache.rb
@@ -1,40 +1,68 @@
+require 'byebug'
+
 class LRUCache
     def initialize(size)
-        @cache = Array.new(size, nil)
-        @last_item_added = nil
-        @lru = nil
+        @cache = Array.new
+        @size = size
     end
 
     def count
         # returns number of elements currently in cache
-        cache.count { |x| !x.nil? }
+        cache.count
     end
 
     def add(el)
       # adds element to cache according to LRU principle
-        cache.each do |ele|
-            if ele.nil?
-                ele = el
-                break
-            end
-        end
-        
-        if cache.all? { |ele| ele.nil? }
-            if lru.nil?
-                cache[0] = el
-                lru = cache[1]
-            end
+
+        if cache.include?(el)
+            debugger
+            # If the cache already contains the element...
+            # Delete the existing element and add the new one to the back
+            cache.delete(el)
+            cache << el
+
+        elsif count >= size
+            debugger
+            # If the cache's size is at its limit...
+            # Take off the first element and add the element to the back
+            # (presumably the first element is the LRU?)
+            cache.shift(1)
+            cache << el
+        else
+            debugger
+            # If neither of these apply, just add the element
+            cache << el
         end
 
-        #cache.push(el) unless cache.none? { |ele| ele.nil? }
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p cache
     end
 
     private
     # helper methods go here!
-    attr_accessor :cache, :last_item_added, :lru
+    attr_accessor :cache
+    attr_reader :size
 
   end
+
+  johnny_cache = LRUCache.new(4)
+
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
+
+  johnny_cache.count # => returns 2
+
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add({a: 1, b: 2, c: 3})
+
+  johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
