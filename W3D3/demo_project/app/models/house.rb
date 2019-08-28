13109eea87e056d5a561663662abdaa97773557e
Danny Chan <dchan6513@gmail.com>
2019-08-28 00:29:07 -0400

    Fix association in House model and successfully test sample queries

--- W3D3/demo_project/app/models/house.rb
+++ W3D3/demo_project/app/models/house.rb
@@ -2,7 +2,7 @@ class House < ApplicationRecord
     has_many(
         :residents,
         class_name: 'Person',
-        foreign_key: 'house_id',
-        primary_key: 'id'
+        foreign_key: :house_id,
+        primary_key: :id
     )
 end
