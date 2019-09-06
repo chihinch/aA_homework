d42033792033ac447ddd5f47dc9f305438c2b958
Danny Chan <dchan6513@gmail.com>
2019-09-05 23:39:20 -0400

    Add User controller specs

--- W4D5/spec/controllers/users_controller_spec.rb
+++ W4D5/spec/controllers/users_controller_spec.rb
@@ -11,14 +11,25 @@ RSpec.describe UsersController, type: :controller do
 
   describe "POST #create" do
     context "with invalid params" do
-      it "validates the presence of the user's email and password"
+        it "validates the presence of the user's email and password" do
+            post :create, params: { user: { email: 'tomato@yucky.com' } }
+            expect(response).to render_template('new')
+            expect(flash[:errors]).to be_present
+        end
 
-      it "validates that the password is at least 6 characters long"
+        it "validates that the password is at least 6 characters long" do
+            post :create, params: { user: { email: 'tomato@yucky.com', password: '1234' } }
+            expect(response).to render_template('new')
+            expect(flash[:errors]).to be_present
+        end
     end
 
     context "with valid params" do
-      it "redirects user to bands index on success"
+        it "redirects user to bands index on success" do
+            post :create, params: { user: { email: 'tomato@yucky.com', password: '1234567' } }
+            expect(response).to redirect_to(user_url(User.last))
+        end
     end
   end
-  
+
 end
