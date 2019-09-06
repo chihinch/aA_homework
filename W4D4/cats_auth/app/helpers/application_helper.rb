5c914696b6c8430d70948efe0dfa39154fa518d2
Danny Chan <dchan6513@gmail.com>
2019-09-05 22:35:48 -0400

    Save changes from W4D4 homework

--- W4D4/cats_auth/app/helpers/application_helper.rb
+++ W4D4/cats_auth/app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
 
     def auth_token
-        html = '<input '
-        html += 'type="hidden" '
-        html += 'name="authenticity_token" '
-        html += 'value="#{ form_authenticity_token }"'
+        html = '<input type="hidden" name="authenticity_token" value="#{ form_authenticity_token }"'
         html.html_safe
     end
 
--- W4D4/cats_auth/app/mailers/user_mailer.rb
+++ W4D4/cats_auth/app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
     def welcome_email(user)
         @user = user
         @url = 'http://example.com'
-        mail(to: user.email, subject: 'Welcome to 99 Cats!')
+        mail(to: 'chihinch@buffalo.edu', subject: 'Welcome to 99 Cats!')
     end
 end
