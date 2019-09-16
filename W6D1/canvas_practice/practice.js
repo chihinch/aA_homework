6017ee5636961e32cba9fae6eeb7f55f71df2305
Danny Chan <dchan6513@gmail.com>
2019-09-15 21:18:49 -0400

    Complete Canvas exercise

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvasElement = document.getElementById("mycanvas");
+  canvasElement.width = 500;
+  canvasElement.height = 500;
+  const ctx = canvasElement.getContext('2d');
 
+  // Draw a rectangle
+  ctx.fillStyle = "grey";
+  ctx.fillRect(0, 0, 200, 150);
+
+  // Draw a circle
+  ctx.beginPath();
+  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.strokeStyle = "red";
+  ctx.stroke();
+  ctx.fillStyle = "red";
+  ctx.fill();
+  
+  // Draw my favourite shape
+  ctx.beginPath();
+  ctx.moveTo(250, 250);
+  ctx.bezierCurveTo(230, 384, 299, 312, 50, 50);
+  ctx.bezierCurveTo(124, 165, 95, 82, 23, 68);
+  ctx.closePath();
+  ctx.strokeStyle = "black"
+  ctx.stroke();
+  ctx.fillStyle = "orange";
+  ctx.fill();
 });
+
+
+
+
