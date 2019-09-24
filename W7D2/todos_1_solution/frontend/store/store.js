81acf00b5a61419702f4c75fb4df3e78c3f16dff
Danny Chan <dchan6513@gmail.com>
2019-09-23 23:31:25 -0400

    Call applyMiddleware in creation of store

--- W7D2/todos_1_solution/frontend/store/store.js
+++ W7D2/todos_1_solution/frontend/store/store.js
@@ -1,12 +1,25 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import { applyMiddleware } from 'redux';
+
+// PHASE 3: Redux
+// Moved the middleware function from Phase 2 to this file
+const addLoggingToDispatch = (store) => (next) => (action) => {
+  console.log(store.getState());
+  console.log(action);
+  let result = next(action);
+  console.log(store.getState());
+  return result;
+};
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  // Add call to applyMiddleware, passing in the middleware, to arguments of createStore
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
 }
+
 
 export default configureStore;
--- W7D2/todos_1_solution/frontend/todo_redux.jsx
+++ W7D2/todos_1_solution/frontend/todo_redux.jsx
@@ -4,7 +4,6 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-
 // PHASE 1: addLoggingToDispatch
 // const addLoggingToDispatch = (store) => {
 //   const dispatch = store.dispatch;
@@ -16,29 +15,33 @@ import Root from './components/root';
 //   };
 // };
 
-// PHASE 2: Refactor to middleware
-const addLoggingToDispatch = (store) => (next) => (action) => {
-  console.log(store.getState());
-  console.log(action);
-  let result = next(action);
-  console.log(store.getState());
-  return result;
-};
+// -------------------------------------------------------------------------- //
 
-const applyMiddlewares = (store, ...middlewares) => {
-  let dispatch = store.dispatch;
-  middlewares.forEach((middleware) => {
-    dispatch = middleware(store)(dispatch);
-  });
-  return Object.assign({}, store, { dispatch });
-};
+// PHASE 2: Refactor to middleware
+// const addLoggingToDispatch = (store) => (next) => (action) => {
+//   console.log(store.getState());
+//   console.log(action);
+//   let result = next(action);
+//   console.log(store.getState());
+//   return result;
+// };
+
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach((middleware) => {
+//     dispatch = middleware(store)(dispatch);
+//   });
+//   return Object.assign({}, store, { dispatch });
+// };
+
+// -------------------------------------------------------------------------- //
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   let store = configureStore(preloadedState);
-  
-  store = applyMiddlewares(store, addLoggingToDispatch);
+
+  // store = applyMiddlewares(store, addLoggingToDispatch);
   // store.dispatch = addLoggingToDispatch(store);
 
   const root = document.getElementById('content');
