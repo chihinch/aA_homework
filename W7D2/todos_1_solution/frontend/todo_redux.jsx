87dbccbbb93d342657cc813a1bda04f25493f989
Danny Chan <dchan6513@gmail.com>
2019-09-23 23:20:53 -0400

    Convert addLoggingToDispatch to middleware

--- W7D2/todos_1_solution/frontend/todo_redux.jsx
+++ W7D2/todos_1_solution/frontend/todo_redux.jsx
@@ -4,22 +4,43 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = (store) => {
-  const dispatch = store.dispatch;
-  return (action) => {
-    console.log(store.getState()); // log the old state
-    console.log(action); // log the action
-    dispatch(action); // call dispatch, passing it the action
-    console.log(store.getState()); // log the new state
-  };
+
+// PHASE 1: addLoggingToDispatch
+// const addLoggingToDispatch = (store) => {
+//   const dispatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState()); // log the old state
+//     console.log(action); // log the action
+//     dispatch(action); // call dispatch, passing it the action
+//     console.log(store.getState()); // log the new state
+//   };
+// };
+
+// PHASE 2: Refactor to middleware
+const addLoggingToDispatch = (store) => (next) => (action) => {
+  console.log(store.getState());
+  console.log(action);
+  let result = next(action);
+  console.log(store.getState());
+  return result;
+};
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
-  store.dispatch = addLoggingToDispatch(store);
+  let store = configureStore(preloadedState);
   
+  store = applyMiddlewares(store, addLoggingToDispatch);
+  // store.dispatch = addLoggingToDispatch(store);
+
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
