d88eb80c649a2463e3f576de865bb4753e9a5656
Danny Chan <dchan6513@gmail.com>
2019-09-25 00:28:23 -0400

    Implement store and test change of state

--- W7D3/giphy/giphy_search.jsx
+++ W7D3/giphy/giphy_search.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import fetchSearchGiphys from './util/api_util';
+import { receiveSearchGiphys } from  './actions/giphy_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
-  // test that fetchSearchGiphys works
-  // window.fetchSearchGiphys = fetchSearchGiphys;
+  const store = configureStore();
+  window.store = store;
+  window.fetchSearchGiphys = fetchSearchGiphys;
+  window.receiveSearchGiphys = receiveSearchGiphys;
+
 });
